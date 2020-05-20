32ebfe22368e29a42d0726f3c42c169d49d04a7b
Rie Urata <ririson7@gmail.com>
2020-05-20 12:58:05 +0900

    routingの設定戻した

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:dietrecipeflutter/routes.dart';
-import 'package:dietrecipeflutter/pages/height_input.dart';
-import 'package:dietrecipeflutter/pages/target_weight_input.dart';
-import 'package:dietrecipeflutter/pages/weight_input.dart';
-import 'package:dietrecipeflutter/pages/setting.dart';
-import 'package:dietrecipeflutter/pages/calendar.dart';
-import 'package:dietrecipeflutter/pages/day_result.dart';
+import 'package:dietrecipeflutter/routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,17 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (BuildContext context) => HeightInputPage(),
-        '/height_input': (BuildContext context) => HeightInputPage(),
-        '/target_weight_input': (BuildContext context) => TargetWeightInputPage(),
-        '/weight_input': (BuildContext context) => WeightInputPage(),
-        '/calendar': (BuildContext context) => CalendarPage(),
-        '/setting': (BuildContext context) => SettingPage(),
-        '/day_result': (BuildContext context) => DayResultPage(),
-      }
-      // routes: routes,
+      routes: routes,
     );
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,18 +1,18 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// import 'package:dietrecipeflutter/pages/height_input.dart';
-// import 'package:dietrecipeflutter/pages/target_weight_input.dart';
-// import 'package:dietrecipeflutter/pages/weight_input.dart';
-// import 'package:dietrecipeflutter/pages/setting.dart';
-// import 'package:dietrecipeflutter/pages/calendar.dart';
-// import 'package:dietrecipeflutter/pages/day_result.dart';
+import 'package:dietrecipeflutter/pages/height_input.dart';
+import 'package:dietrecipeflutter/pages/target_weight_input.dart';
+import 'package:dietrecipeflutter/pages/weight_input.dart';
+import 'package:dietrecipeflutter/pages/setting.dart';
+import 'package:dietrecipeflutter/pages/calendar.dart';
+import 'package:dietrecipeflutter/pages/day_result.dart';
 
-// final routes = {
-//   '/': (BuildContext context) => HeightInputPage(),
-//   '/height_input': (BuildContext context) => HeightInputPage(),
-//   '/target_weight_input': (BuildContext context) => TargetWeightInputPage(),
-//   '/weight_input': (BuildContext context) => WeightInputPage(),
-//   '/calendar': (BuildContext context) => CalendarPage(),
-//   '/setting': (BuildContext context) => SettingPage(),
-//   '/day_result': (BuildContext context) => DayResultPage(),
-// };
+final routes = {
+  '/': (BuildContext context) => HeightInputPage(),
+  '/height_input': (BuildContext context) => HeightInputPage(),
+  '/target_weight_input': (BuildContext context) => TargetWeightInputPage(),
+  '/weight_input': (BuildContext context) => WeightInputPage(),
+  '/calendar': (BuildContext context) => CalendarPage(),
+  '/setting': (BuildContext context) => SettingPage(),
+  '/day_result': (BuildContext context) => DayResultPage(),
+};
