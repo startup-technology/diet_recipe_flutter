39b5323664db911962e1b99e088acea00ddae25d
jonghoonleeee <jonghoon.leeee@gmail.com>
2020-06-07 18:01:41 +0900

    [Modify] ログイン機能実装

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -56,7 +56,7 @@ class _LoginPageState extends State<LoginPage> {
                     // ログインに成功した場合
                     // final FirebaseUser user = result.user;
                     setState(() {
-                      Navigator.of(context).pushNamed("/target_weight_input");
+                      Navigator.of(context).pushNamed("/height_input");
                       // infoText = "ログインOK：${user.email}";
                     });
                   } catch (e) {
--- lib/routes.dart
+++ lib/routes.dart
@@ -2,6 +2,7 @@ import 'package:dietrecipeflutter/pages/top.dart';
 import 'package:flutter/material.dart';
 
 import 'package:dietrecipeflutter/pages/height_input.dart';
+import 'package:dietrecipeflutter/pages/weight_input.dart';
 import 'package:dietrecipeflutter/pages/target_weight_input.dart';
 import 'package:dietrecipeflutter/pages/setting.dart';
 import 'package:dietrecipeflutter/pages/calendar.dart';
@@ -13,6 +14,7 @@ final routes = {
   '/': (BuildContext context) => TopPage(),
   '/height_input': (BuildContext context) => HeightInputPage(),
   '/target_weight_input': (BuildContext context) => TargetWeightInputPage(),
+  '/weight_input': (BuildContext context) => WeightInputPage(),
   '/calendar': (BuildContext context) => CalendarPage(),
   '/setting': (BuildContext context) => SettingPage(),
   '/day_result': (BuildContext context) => DayResultPage(),
