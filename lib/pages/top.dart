9e1dc33167df8eaa00e0da024718867c1923e553
jonghoonleeee <jonghoon.leeee@gmail.com>
2020-06-11 14:01:07 +0900

    [Modify] トップ画面に今日の体重が入力できるボタンを追加

--- lib/pages/top.dart
+++ lib/pages/top.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:dietrecipeflutter/ui/chart/line_chart.dart';
 import 'package:dietrecipeflutter/database/database_helper.dart';
+
 // import 'package:dietrecipeflutter/ui/chart/bar_chart.dart';
 // import 'package:dietrecipeflutter/ui/chart/pie_chart.dart';
-
 class TopPage extends StatefulWidget {
   @override
   _TopPageState createState() => _TopPageState();
@@ -12,22 +12,10 @@ class TopPage extends StatefulWidget {
 class _TopPageState extends State<TopPage> {
   final dbHelper = DatabaseHelper.instance;
   List<double> points = [50, 55, 60, 55, 60, 61, 55];
-  List<String> labels = [
-    "2012",
-    "2013",
-    "2014",
-    "2015",
-    "2016",
-    "2017",
-    "2018",
-    "2019",
-  ];
-
   int targetBodyWeight = 0;
   int bodyWeight = 0;
   int currentHeight = 0;
   int remnantWeight = 0;
-
   _TopPageState() {
     findBodyWeight().then((val) => setState(() {
           bodyWeight = val;
@@ -36,9 +24,8 @@ class _TopPageState extends State<TopPage> {
           currentHeight = val;
         }));
     findTargetBodyWeight().then((val) => setState(() {
-        targetBodyWeight = val;
-      }));
-
+          targetBodyWeight = val;
+        }));
   }
   @override
   Widget build(BuildContext context) {
@@ -50,12 +37,13 @@ class _TopPageState extends State<TopPage> {
           margin: EdgeInsets.all(15.0),
           child: Column(
             children: <Widget>[
-              _targetWeight(targetBodyWeight), _currentWeight(currentHeight, bodyWeight, targetBodyWeight),
+              _targetWeight(targetBodyWeight),
+              _currentWeight(currentHeight, bodyWeight, targetBodyWeight),
               Text('体重変動', style: TextStyle(fontSize: 20.0)), // 3.1.1
               Container(
                 margin: EdgeInsets.only(top: 15.0),
                 child: CustomPaint(
-                  size: Size(240, 240),
+                  size: Size(200, 150),
                   painter: LineChart(
                       points: points,
                       pointSize: 5.0,
@@ -64,6 +52,17 @@ class _TopPageState extends State<TopPage> {
                       lineWidth: 2.0),
                 ),
               ),
+              RaisedButton(
+                child: Text("今日の体重を入力"),
+                color: Colors.blue,
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/weight_input');
+                },
+              ),
             ],
           )),
     );
@@ -71,7 +70,7 @@ class _TopPageState extends State<TopPage> {
 
   Widget _targetWeight(targetBodyWeight) {
     return Container(
-        margin: EdgeInsets.only(top: 30.0, bottom: 30.0),
+        margin: EdgeInsets.only(top: 10.0, bottom: 30.0),
         child: Row(
           // 1行目
           children: <Widget>[
@@ -88,10 +87,8 @@ class _TopPageState extends State<TopPage> {
                   ),
                   Container(
                     // 3.1.2行目
-                    child: Text(
-                      targetBodyWeight.toString() + ' kg',
-                      style: TextStyle(fontSize: 25.0)
-                    ),
+                    child: Text(targetBodyWeight.toString() + ' kg',
+                        style: TextStyle(fontSize: 25.0)),
                   ),
                   Container(
                     margin: EdgeInsets.only(top: 30.0, right: 15.0, left: 15.0),
@@ -117,7 +114,8 @@ class _TopPageState extends State<TopPage> {
       children: <Widget>[
         _weightColumn(bodyWeight.toString(), "体重"), // 2.1
         _heightColumn(currentHeight.toString(), "身長"), // 2.2
-        _changeWeightColumn((bodyWeight-targetBodyWeight).toString(), "体重増減") // 2.3
+        _changeWeightColumn(
+            (bodyWeight - targetBodyWeight).toString(), "体重増減") // 2.3,
       ],
     ));
   }
@@ -128,12 +126,12 @@ class _TopPageState extends State<TopPage> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Row(children: <Widget>[
-          Text(physical, style: TextStyle(fontSize: 25.0)), 
+          Text(physical, style: TextStyle(fontSize: 25.0)),
           Text('  kg', style: TextStyle(fontSize: 15.0)),
         ]),
         Container(
           // 3.1.2
-          margin: const EdgeInsets.only(top: 8.0, bottom: 40.0),
+          margin: const EdgeInsets.only(top: 8.0, bottom: 20.0),
           child: Text(
             label,
             style: TextStyle(
@@ -145,19 +143,19 @@ class _TopPageState extends State<TopPage> {
       ],
     );
   }
-  
+
   Widget _heightColumn(String physical, String label) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Row(children: <Widget>[
-          Text(physical, style: TextStyle(fontSize: 25.0)), 
+          Text(physical, style: TextStyle(fontSize: 25.0)),
           Text('  cm', style: TextStyle(fontSize: 15.0)),
         ]),
         Container(
           // 3.1.2
-          margin: const EdgeInsets.only(top: 8.0, bottom: 40.0),
+          margin: const EdgeInsets.only(top: 8.0, bottom: 0.0),
           child: Text(
             label,
             style: TextStyle(
@@ -169,13 +167,14 @@ class _TopPageState extends State<TopPage> {
       ],
     );
   }
+
   Widget _changeWeightColumn(String physical, String label) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Row(children: <Widget>[
-          Text(physical, style: TextStyle(fontSize: 25.0, color: Colors.red)), 
+          Text(physical, style: TextStyle(fontSize: 25.0, color: Colors.red)),
           Text('  kg', style: TextStyle(fontSize: 15.0)),
         ]),
         Container(
@@ -197,24 +196,23 @@ class _TopPageState extends State<TopPage> {
     DateTime now = DateTime.now();
     var query;
     query = await dbHelper.queryRows(
-      table: 'body_weights',
-      where: 'DATE(input_date) =',
-      whereArgs: "DATE('$now')"
-    );
-    return query.first['body_weight'];
+        table: 'body_weights',
+        where: 'DATE(input_date) =',
+        whereArgs: "DATE('$now')");
+    return query.last['body_weight'];
   }
 
   Future<int> findCurrentHeight() async {
     var query;
     query = await dbHelper.queryRowLast('current_heights');
     print(query);
-    return query.first['current_height'];
+    return query.last['current_height'];
   }
 
   Future<int> findTargetBodyWeight() async {
     var query;
     query = await dbHelper.queryRowLast('target_body_weights');
     print(query);
-    return query.first['body_weight'];
+    return query.last['body_weight'];
   }
 }
